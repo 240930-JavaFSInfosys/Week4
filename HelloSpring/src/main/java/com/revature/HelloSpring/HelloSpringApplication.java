1f38942235318f8616ff764594fc4bee14d27780
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-21 14:31:54 -0400

    Quick example of dependency injection with spring!

--- HelloSpring/src/main/java/com/revature/HelloSpring/HelloSpringApplication.java
+++ HelloSpring/src/main/java/com/revature/HelloSpring/HelloSpringApplication.java
@@ -1,13 +1,39 @@
 package com.revature.HelloSpring;
 
+import com.revature.models.Pizza;
+import com.revature.models.Topping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
+
+
+		//TODO: talk about our ApplicationContext object
+
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+		//Ask for a Topping bean and print it out
+		Topping t = ac.getBean(Topping.class);
+		System.out.println(t);
+
+		//Nothing super interesting here^ we just have a Topping object with default values
+
+		//Ask for a Pizza bean and print it out
+		Pizza p = ac.getBean(Pizza.class);
+		System.out.println(p);
+
+		//WOW! We have a Pizza object with a Topping object DEPENDENCY INJECTED for us!
+		//It was instantiated and set for us by Spring, since we made Topping a dependency of Pizza
+
+		//TODO: give some values to these after talking notes and comments
+
 	}
 
 }
